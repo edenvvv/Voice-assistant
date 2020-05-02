838a554a5d9287b6fe06084fc18c5f20ce67624a
eden dadon <edenvvv@gmail.com>
2020-05-02 03:02:59 +0300

    the base code

---
+++ speech_to_text.py
@@ -0,0 +1,22 @@
+"""
+import library:
+pip install pipwin
+pipwin install pyaudio
+"""
+import speech_recognition as sr
+
+# Initialize recognizer class (for recognizing the speech)
+r = sr.Recognizer()
+
+# Reading Microphone as source
+# listening the speech and store in audio_text variable
+with sr.Microphone() as source:
+    print("Talk please")
+    audio_text = r.listen(source)
+    print("thanks ╰(*°▽°*)╯")
+
+    try:
+        # using google speech recognition
+        print(f"Text: {r.recognize_google(audio_text)} ")
+    except sr.UnknownValueError:
+        print("Sorry, I did not get that")
