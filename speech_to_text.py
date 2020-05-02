f99e5cd6028639f890c1bf080ee131959107575f
eden dadon <edenvvv@gmail.com>
2020-05-02 23:48:11 +0300

    .

--- speech_to_text.py
+++ speech_to_text.py
@@ -12,23 +12,43 @@ def input_text():
     """
     import speech_recognition as sr
     # Initialize recognizer class (for recognizing the speech)
-    r = sr.Recognizer()
+    recorder = sr.Recognizer()
     # Reading Microphone as source
     # listening the speech and store in audio_text variable
     with sr.Microphone() as source:
         while True:
             print("Talk please")
-            audio_text = r.listen(source)
+            audio_text = recorder.listen(source)
             print("thanks ╰(*°▽°*)╯")
 
             try:
                 # using google speech recognition
-                print(f"Text: {r.recognize_google(audio_text)} ")
-                return r.recognize_google(audio_text)
+                print(f"Text: {recorder.recognize_google(audio_text)} ")
+                return recorder.recognize_google(audio_text)
             except sr.UnknownValueError:
                 print("Sorry, I did not get that, please try again.")
 
 
+def translate(name):
+    from googletrans import Translator
+    """
+    LANGUAGES:
+    import googletrans
+    print(googletrans.LANGUAGES)
+    """
+    translator = Translator()
+    # English -> Hebrew
+    result = translator.translate(name, 'he', 'en')
+    print(result.text)
+    """
+    Hebrew -> English
+    result2 = translator.translate(name, 'en', 'he')
+    print(result2.text)
+    """
+    return result.text
+
+
 if __name__ == "__main__":
     search_name = input_text()
-    google_search(search_name)
+    # google_search(search_name)
+    translate(search_name)
